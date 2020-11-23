99d46f9a45f2c1ad6cce18dd096e0faac1d12c9e
Ohne <ohne68@hotmail.fr>
2020-11-23 03:48:41 +0100

    Messages d'erreur ajoutés

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,94 +47,57 @@ export class AppComponent implements OnInit {
 
   edit(event:any, userId:number, operation:string) {
     // La vérification de l'input se fait au moment où amountToTransmit est affectée, dans la fonction getAmount() se déclenchant à la saisie
-    // Ici, on vérifie donc que cette valeur n'est pas vide
+    // Ici, on vérifie donc que cette valeur n'est pas vide avant d'envoyer la requête
     if (this.amountToTransmit) {
-      this.dataService.editWallet(userId, operation, this.amountToTransmit).subscribe((res: User) => {
-        // Une fois la requête envoyée et la réponse reçue, on modifie notre variable users et cela modifiera automatiquement le DOM
-       
-        // Mais d'abord, on affiche un petit message
+      // On cherche l'index de l'utilisateur correspondant (grâce à la réponse du serveur) et on va l'utiliser pour certaines opérations
+      let index = this.users.findIndex(user => user.id == userId);
+      // On vérifie que, dans le cas où l'opération est un débit, la porte-feuille ne soit pas négatif (l'API renverrait une erreur de toute manière)
+      if (((this.users[index].wallet - this.amountToTransmit) < 0) && (operation === "-")) {
+        // Dans ce cas, on indique cette nécessité
+        let tooltip = event.target.parentNode.parentNode.lastElementChild;
+        let textNode = document.createTextNode("Vous ne pouvez pas être à découvert, veuillez entrer un montant à débiter moins élevé");
+        tooltip.append(textNode);
+        setTimeout(() => { tooltip.style.opacity = "1" }, 1);
+        setTimeout(() => { tooltip.style.opacity = "0" }, 2001);
+        setTimeout(() => { tooltip.removeChild(tooltip.firstChild) }, 3001);
+      }
+      else{
+        this.dataService.editWallet(userId, operation, this.amountToTransmit).subscribe((res: User) => {
+          // Une fois la requête envoyée et la réponse reçue, on modifie notre variable users et cela modifiera automatiquement le DOM
 
-        // Pendant cette animation de 3s, on empêche l'utilisateur de renvoyer des requêtes
-        // TO DO
-        // TO DO
-        // Désactiver les boutons
-        // TO DO
-        // TO DO
-        let message = document.createElement("div");
-        let text = "La somme de "+this.amountToTransmit+"€ a bien été";
-        text += (operation === "+") ? " ajoutée au " : " retirée du ";
-        text += "porte-feuille de "+res.firstname+ " "+res.name+".";
-        let textNode = document.createTextNode(text);
-        message.append(textNode);
-        // TO DO
-        // TO DO
-        // La position absolute n'est pas la bonne solution car en cas de redimensionnement de la fenêtre...
-        // TO DO
-        // TO DO
-        message.style.position = "absolute";
-        // On donne maintenant une position à toolTip, à côté du tableau
-        message.style.top = event.target.getBoundingClientRect().top - 3 + "px";
-        if (event.target.innerText === "+") {
-          message.style.left = event.target.getBoundingClientRect().left + 70 + "px";
-        }
-        else {
-          message.style.left = event.target.getBoundingClientRect().left + 40 + "px";
-        }
-        message.style.backgroundColor = "rgb(200, 200, 200)";
-        message.style.fontSize = "0.9em";
-        message.style.padding = "5px";
+          // Mais d'abord, on va désactiver les boutons car on va afficher un message dans une petite animation de 3 secondes
+          let enfants = Array.from(event.target.parentNode.childNodes);
+          enfants = enfants.filter((enfant: Node) => enfant.nodeName === "BUTTON");
+          enfants.forEach((enfant: any) => { enfant.disabled = true });
 
-        // On va le faire apparaître en fondu en lui donnant une opacité à 0
-        message.style.opacity = "0";
-        message.style.transition = "opacity 1s"
+          // Ensuite va afficher le message de validation dans l'élément créé à cet effet
+          let tooltip = event.target.parentNode.parentNode.lastElementChild;
+          let text = "La somme de " + this.amountToTransmit + "€ a bien été";
+          text += (operation === "+") ? " ajoutée au " : " retirée du ";
+          text += "porte-feuille de " + res.firstname + " " + res.name + ".";
+          let textNode = document.createTextNode(text);
+          tooltip.append(textNode);
 
-        event.target.parentNode.append(message);
+          setTimeout(() => { tooltip.style.opacity = "1" }, 1);
+          setTimeout(() => { tooltip.style.opacity = "0" }, 2000);
+          setTimeout(() => {
 
-        setTimeout(() => { message.style.opacity = "1" }, 1);
-        setTimeout(() => { message.style.opacity = "0"}, 2000);
-        setTimeout(() => { 
-          // On cherche l'index de l'utilisateur correspondant (grâce à la réponse du serveur) et on va l'utiliser pour modifier notre tableau d'utilisateurs
-          let index = this.users.findIndex(user => user.id == userId);
-          this.users[index] = res;
-          // Cette modification entraîne un rerendu du DOM qui effacera automatiquement le message
-        }, 3000);
-      });
+            this.users[index] = res;
+            // Cette modification entraîne un rerendu du DOM qui effacera automatiquement le message et réactivera les boutons
+          }, 3000);
+        });
+      }
     }
-    // Si la valeur est vide, c'est que l'utilisateur n'a pas saisi de nombre, et on lui indique
+    
     else{
+      // Si la valeur est vide, c'est que l'utilisateur n'a pas saisi de nombre, et on lui indique
       // Mais seulement si ce message n'existe pas déjà^
-      let tooltip = event.target.parentNode.lastElementChild;
+      let tooltip = event.target.parentNode.parentNode.lastElementChild;
       if (!tooltip.id.includes("tooltip")) {
         // On va afficher un message disant d'entrer un nombre en tooltip
-        let toolTip = document.createElement("div");
-        let text = document.createTextNode("Veuillez entrer un nombre")
-        toolTip.append(text);
-        toolTip.id = "tooltip" + this.numberToolTipId++;
-        // TO DO
-        // TO DO
-        // La position absolute n'est pas la bonne solution car en cas de redimensionnement de la fenêtre...
-        // TO DO
-        // TO DO
-        toolTip.style.position = "absolute";
-        // On donne maintenant une position à toolTip, à côté du tableau
-        toolTip.style.top = event.target.getBoundingClientRect().top - 3 + "px";
-        if (event.target.innerText === "+") {
-          toolTip.style.left = event.target.getBoundingClientRect().left + 70 + "px";
-        }
-        else {
-          toolTip.style.left = event.target.getBoundingClientRect().left + 40 + "px";
-        }
-        toolTip.style.backgroundColor = "rgb(200, 200, 200)";
-        toolTip.style.fontSize = "0.9em";
-        toolTip.style.padding = "5px";
-
-        // On va le faire apparaître en fondu en lui donnant une opacité à 0
-        toolTip.style.opacity = "0";
-        toolTip.style.transition = "opacity 1s"
-
-        event.target.parentNode.append(toolTip);
-
-        setTimeout(() => { toolTip.style.opacity = "1" }, 1);
+        let textNode = document.createTextNode("Veuillez entrer un nombre");
+        tooltip.append(textNode);
+        setTimeout(() => { tooltip.style.opacity = "1" }, 1);
       }
     }
   }
@@ -146,14 +109,11 @@ export class AppComponent implements OnInit {
     if (!isNaN(value)){
       this.amountToTransmit = event.target.value;
       // Si un tooltip avertissant d'entrer un nombre était présent, on le supprime
-      let tooltip = event.target.parentNode.lastElementChild;
-      // Si l'id de cet élément contient "tooltip", c'est bien le tooltip
-      if (tooltip.id.includes("tooltip")){
+      let tooltip = event.target.parentNode.parentNode.lastElementChild;
+      if (tooltip.childNodes.length>0){
         // Pour le faire disparaître progressivement
         setTimeout(() => { tooltip.style.opacity = "0" }, 1);
-        setTimeout(() => {
-          event.target.parentNode.removeChild(tooltip);
-        }, 1000);
+        setTimeout(() => { tooltip.removeChild(tooltip.firstChild) }, 1001);
       }
     }
     // De plus, si la valeur n'est pas un nombre, elle réinitialise amountToTransmit à null, car il suffit d'un seul caracatère non-numérique pour provoquer un bug
@@ -162,4 +122,5 @@ export class AppComponent implements OnInit {
     }
   }
 
+  
 }
